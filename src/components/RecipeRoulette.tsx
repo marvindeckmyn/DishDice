b64a53d8aa40d81a9d1113cadd86621aa03bf7fa
marvindeckmyn <marvindeckmyn@gmail.com>
2024-11-26 23:07:24 +0100

    Initial commit: DishDice base setup

---
+++ src/components/RecipeRoulette.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react';
+
+const RecipeRoulette = () => {
+    const [ingredients, setIngredients] = useState([]);
+
+    return (
+        <div className="max-w-2xl mx-auto p-4 space-y-6">
+            Hello World
+        </div>
+    );
+};
+
+export default RecipeRoulette;
